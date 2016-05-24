b3ec1e51e1f5edc730b6b901ba16701d34d56808
daveborg <dborgeest@rallydev.com>
2016-05-24 15:20:14 +1000

    ugly version, but working

--- App.js
+++ App.js
@@ -3,6 +3,29 @@ Ext.define('CustomApp', {
     componentCls: 'app',
     items:{ html:'<a href="https://help.rallydev.com/apps/2.0rc3/doc/">App SDK 2.0rc3 Docs</a>'},
     launch: function() {
-        //Write app code here
+        console.log('Hello big bad world!!!');
+        
+        var myStore = Ext.create('Rally.data.wsapi.Store', {
+    		model: 'User Story',
+    		autoLoad: true,
+    		listeners: {
+        		load: function(store, data, success) {
+            		console.log('Got the data! I did I did!', store, data, success);
+            		
+            		var myGrid = Ext.create('Rally.ui.grid.Grid', {
+            			store: myStore,
+            			columnCfgs: [
+            				'FormattedID', 'Name', 'ScheduleState'
+            			]
+            		});
+            		
+            		this.add(myGrid);
+            		
+        		},
+        		scope: this
+    		},
+    		
+    		fetch: ['Name', 'ScheduleState', 'FormattedID']
+		});
     }
 });
